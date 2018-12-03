e96251240ac170266c0f7a419d7294236e18f739
hekf2021 <hekf2021@126.com>
2018-12-03 23:13:18 +0800

    功能性提交

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("id","title","content")
 # Register your models here.
+admin.site.register(Article,ArticleAdmin)
